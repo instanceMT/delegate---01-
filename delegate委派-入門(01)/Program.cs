7daa7055168358461bb4f466ef328ffb276177f4
King Liu <twmland@gmail.com>
2023-11-22 03:03:53 +0800

    加入專案檔案。

---
+++ delegate委派-入門(01)/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace delegate委派_入門_01_
+{
+    //在 namespace 定義委派
+    public delegate void delegate1();
+    internal class Program
+    {
+        //在類別定義委派
+        public delegate int delegate2(int a);
+        static void Main(string[] args)
+        {
+            delegate1 d1 = (new Ex()).func1;
+            delegate2 d2 = Ex.func2;
+            //delegate1 d1 = new delegate1((new Ex()).func1);
+            //delegate2 d2 = new delegate2(Ex.func2);
+
+            #region delegate1
+            d1();
+            d1.Invoke();
+            #endregion
+
+            #region delegate2
+            var s1 = d2.Invoke(2);
+            var s2 = d2(3);
+            Console.WriteLine($"s1={s1}");
+            Console.WriteLine($"s2={s2}");
+            #endregion
+
+            Console.ReadKey();
+        }
+    }
+   
+    public class Ex
+    {
+        public void func1()
+        {
+            Console.WriteLine($"{nameof(func1)}被呼叫");
+        }
+
+        public static int func2(int a)
+        {
+            return a * a;
+        }
+    }
+}
